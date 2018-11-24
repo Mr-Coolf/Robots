8ea16397ec749d164fa469f62793cc8532d0aab1
Mr-Coolf <44525782+Mr-Coolf@users.noreply.github.com>
2018-11-24 11:27:39 +0200

    сумашедший робот который идёт назад

---
+++ choknutui_robot.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+Servo ar;
+Servo se;
+int pos=0;
+void setup() {
+  ar.attach(9);
+  se.attach(6);
+}
+
+void loop() {
+  for(pos = 1; pos <= 180; pos += 1) {
+    ar.write(pos);
+    se.write(pos);
+    delay(5);
+  }
+  for(pos = 180; pos >= 1; pos -= 1) {
+    ar.write(pos);
+    se.write(pos);
+    delay(5);
+  }
+}
