092fc604543ec3307757848e48bf4f942ce66aea
Mr-Coolf <44525782+Mr-Coolf@users.noreply.github.com>
2018-11-24 11:34:21 +0200

    робот хочет ити назад чуть чуть чоконутый

---
+++ robot_hochet_iti_nazad.ino
@@ -0,0 +1,18 @@
+#include <Servo.h>
+Servo motor1;
+Servo motor2;
+
+void setup() {
+  motor1.attach(9);
+  motor2.attach(6);
+}
+
+void loop() {
+  motor1.write(60);
+  motor2.write(120);
+  delay(300);
+
+  motor1.write(120);
+  motor2.write(60);
+  delay(300);
+}
