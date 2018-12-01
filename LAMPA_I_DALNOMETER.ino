e85b711ea88d8ca2c1adda37cc5726bad5f95c03
Mr-Coolf <44525782+Mr-Coolf@users.noreply.github.com>
2018-12-01 12:09:38 +0200

    если расстояние меньше десяти лампочка вкл

---
+++ LAMPA_I_DALNOMETER.ino
@@ -0,0 +1,27 @@
+int eP = 9;
+int tP = 8;
+int led = 3;
+
+void setup() {
+  pinMode(tP, OUTPUT);
+  pinMode(eP, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(2000000);
+}
+
+void loop() {
+  long time, distance;
+  digitalWrite(tP, LOW);
+  delayMicroseconds(2);
+  digitalWrite(tP, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(tP, LOW);
+  time = pulseIn(eP, HIGH);
+  distance = (time / 58);
+  Serial.println(distance);
+  if (distance < 10) {
+    digitalWrite(led, HIGH);
+  }
+  else
+    digitalWrite(led, LOW);
+}
