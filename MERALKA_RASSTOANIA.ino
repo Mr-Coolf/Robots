e0151d0d75e4ea7a6f88520ffd275272991e3924
Mr-Coolf <44525782+Mr-Coolf@users.noreply.github.com>
2018-12-01 12:10:18 +0200

    вещь которая меряет расстояние меряет

---
+++ MERALKA_RASSTOANIA.ino
@@ -0,0 +1,18 @@
+#include <NewPing.h>
+
+#define TRIGGER_PIN 11
+#define MAX_DISTANCE 200
+#define ECHO_PIN 12
+
+NewPing arsen(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup() {
+  Serial.begin(2000000);
+}
+
+void loop() {
+  delay(50);
+  unsigned int us = arsen.ping_cm();
+  Serial.print(us);
+  Serial.println("cm");
+}
