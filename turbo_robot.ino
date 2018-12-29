94e7922fe2db73945a5fb237a7dbf8080d9fe4e5
Mr-Coolf <44525782+Mr-Coolf@users.noreply.github.com>
2018-12-29 11:50:54 +0200

    Слишком быстрый...

---
+++ turbo_robot.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+#include <EEPROM.h>
+#include "Otto.h"
+Otto bot;
+#define PIN_YL 2
+#define PIN_YR 3
+#define PIN_RL 4
+#define PIN_RR 5
+
+void setup() {
+  Serial.begin(2000000);
+  bot.init(PIN_YL,PIN_YR,PIN_RL,PIN_RR,true);
+  bot.home();
+}
+
+void loop() {
+  bot.move(1,100,100);
+  bot.move(1,100,100);
+  bot.move(1,100,100);
+  bot.move(1,100,100);
+  bot.move(1,100,100);
+  delay(50);
+  bot.move(2,100,100);
+  bot.move(2,100,100);
+  bot.move(2,100,100);
+  bot.move(2,100,100);
+  bot.move(2,100,100);
+}
